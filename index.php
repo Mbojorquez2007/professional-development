425466f945c2f3b17839e18eb6dc4bc50f1c05f1
Michelle <mbojorquez4@cnm.edu>
2017-07-18 16:25:18 -0600

    put buddy's name on top

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<title>Michelle Allen</title>
 	</head>
 	<body>
-		<h1>Michelle Allen professional development</h1>
+		<h1>Michelle Allen professional development My buddy is Steven Hebert</h1>
 
 		<!-- This is an HTML comment -->
 		<h2>3 quantitative accomplishment</h2>
@@ -12,7 +12,7 @@
 			<li>I was in charge of overseeing hundreds of residential families within UNM housing program</li>
 			<li>I improved corporate efficacy by producing higher revenue in sales , enhanced a higher quality in customer
 				service which resulted in a higher quantity of cliental and modified daily operations.</li>
-			<li>I was a representative in a initial contact for the managerial staff and sole intermediary between hundreds
+			<li>I was a representative and an initial contact for the managerial staff and sole intermediary between hundreds
 				of clients within the primer restaurant business.</li>
 		</ul>
 
@@ -20,7 +20,7 @@
 		<ol>
 			<li>Ambious /timing is everything I'm the first to get there and the last to leave.</li>
 			<li>Accleamated/ easy adaptable from day one</li>
-			<li>Motivated/in being busy and occupied is a motive tool with drives everyone</li>
+			<li>Motivated/ being busy and occupied is a motive tool with drives everyone</li>
 
 		</ol>
 
@@ -29,7 +29,7 @@
 			My name is Michelle. I build apps that help the community.
 			I’m doing this full-stack boot camp with CNM and Im learning front end design and how to create a
 			fully-functional back-end. Are you working on apps?</p>
-		<p>I am a visual learner and my buddy is Steven Hebert</p>
+		<p>I am a visual learner</p>
 		<h2>Golden Circle</h2>
 		<img src="image/golden%20circle.jpg">
 
