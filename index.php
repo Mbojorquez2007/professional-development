8c3cbedff2610fc39a83a36d5ecebb6fd6566562
Michelle <mbojorquez4@cnm.edu>
2017-07-18 11:45:39 -0600

    created my prfile

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head><meta charset="utf-8"/>
+		<title>Michelle Allen</title>
+	</head>
+	<body>
+		<h1>Michelle Allen professional development</h1>
+
+		<!-- This is an HTML comment -->
+		<h2>3 quantitative accomplishment</h2>
+		<ul>
+			<li>I was in charge of overseeing hundreds of residential families within UNM housing program</li>
+			<li>I improved corporate efficacy by producing higher revenue in sales , enhanced a higher quality in customer
+				service which resulted in a higher quantity of cliental and modified daily operations.</li>
+			<li>I was a representative in a initial contact for the managerial staff and sole intermediary between hundreds
+				of clients within the primer restaurant business.</li>
+		</ul>
+
+		<h2>3 qualitative</h2>
+		<ol>
+			<li>Ambious /timing is everything I'm the first to get there and the last to leave.</li>
+			<li>Accleamated/ easy adaptable from day one</li>
+			<li>Motivated/in being busy and occupied is a motive tool with drives everyone</li>
+
+		</ol>
+
+		<h2>Elevator Pitch</h2>
+		<p>Hi
+			My name is Michelle. I build apps that help the community.
+			I’m doing this full-stack boot camp with CNM and Im learning front end design and how to create a
+			fully-functional back-end. Are you working on apps?</p>
+		<p>I am a visual learner and my buddy is Steven Hebert</p>
+		<h2>Golden Circle</h2>
+		<img src="image/golden%20circle.jpg">
+
+
+
+
+
+
