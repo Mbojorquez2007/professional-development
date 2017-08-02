082da98c0ec97a7d56a217722d444da37f99b68c
Michelle <mbojorquez4@cnm.edu>
2017-08-02 07:45:28 -0600

    added more about my teammates

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
 	</head>
 	<body>
 		<h1>Michelle Allen professional development My buddy is Steven Hebert</h1>
-
 		<!-- This is an HTML comment -->
 		<h2>3 quantitative accomplishment</h2>
 		<ul>
 			<li>I was in charge of overseeing hundreds of residential families within UNM housing program</li>
 			<li>I improved corporate efficacy by producing higher revenue in sales , enhanced a higher quality in customer
-				service which resulted in a higher quantity of cliental and modified daily operations.</li>
-			<li>I was a representative and an initial contact for the managerial staff and sole intermediary between hundreds
-				of clients within the primer restaurant business.</li>
+				<1i>service which resulted in a higher quantity of cliental and modified daily operations.</li>
+			<li>I was a representative and an initial contact for the managerial staff and sole intermediary between hundreds of clients within the primer restaurant business.</li>
 		</ul>
 
 		<h2>3 qualitative</h2>
@@ -21,18 +19,17 @@
 			<li>Ambious /timing is everything I'm the first to get there and the last to leave.</li>
 			<li>Accleamated/ easy adaptable from day one</li>
 			<li>Motivated/ being busy and occupied is a motive tool with drives everyone</li>
-
 		</ol>
-
-		<h2>Elevator Pitch</h2>
+		<<h2>Elevator Pitch</h2>
 		<p>Hi
 			My name is Michelle. I build apps that help the community.
 			I’m doing this full-stack boot camp with CNM and Im learning front end design and how to create a
 			fully-functional back-end. Are you working on apps?</p>
 		<p>I am a visual learner</p>
+		<p>Playing forbbien island was a great way to learn about my teammates. At first everyone seemed to be shy and then through out the game eveyone one seemed to lighten up. No one was over bearing or controlling I honestly think we all worked as a team really well helping each other and giving each other advised when needed. </p>
 		<h2>Golden Circle</h2>
 		<img src="image/golden%20circle.jpg">
-
+	tes
 
 
 
